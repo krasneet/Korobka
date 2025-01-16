448c219aabc679101144eb1a5f0d6140bd16d86f
323 <323@323-15>
2025-01-16 12:27:09 +0300

    Добавьте файлы проекта.

---
+++ Box.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Box
+    {
+        private int b; //ширина
+        private int l; //длина
+        private int h; //высота
+
+        public int B
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                if(value > 0) b = value;
+            }
+        }
+
+        public int L
+        {
+            get
+            {
+                return l;
+            }
+            set
+            {
+                if (value > 0) l = value;
+            }
+        }
+
+        public int H
+        {
+            get
+            {
+                return h;
+            }
+            set
+            {
+                if (value > 0) h = value;
+            }
+        }
+
+        public Box()
+        {
+            B = 0;
+            L = 0;
+            H = 0;
+        }
+
+        public Box(int a, int b,int c)
+        {
+            B = a;
+            L = b;
+            H = c;
+        }
+
+        public float Volume()
+        {
+            float v = l*b*h;
+            return v;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Объем коробки ширина = {B}, длина = {L}, высота = {H}");
+            return;
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+
+            box.B = 2;
+            box.L = 5;
+            box.H = 3;
+
+            box.Show();
+
+            float f = box.Volume();
+            Console.WriteLine($"Объем коробки = {f}");
+
+            Console.ReadLine();
+        }
+    }
+}
